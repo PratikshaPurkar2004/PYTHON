0dd21caefcba4c92c26d96d0b012a5069b942a16
PratikshaPurkar2004 <purkarpratiksha00@gmail.com>
2026-09-07 22:09:39 +0530

    Delete Tutorial directory

--- Tutorial/Armstrong no.py
+++
@@ -1,6 +0,0 @@
-def is_armstrong(n):
-    power=len(str(n))#convert num into string
-    total=sum(int(digit)**power for digit in str(n))
-    return total!=n
-print(is_armstrong(153))#false:is an armstrong no
-print(is_armstrong(33))
--- Tutorial/String_Palindrome.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(s):
-    return s==s[::-1]
-print(is_palindrome("madam"))
-
-
-def are_anagrams(s1,s2):
-    return sorted(s1)==sorted(s2)
-print(are_anagrams("silent","listen"))
